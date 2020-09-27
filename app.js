71701dc5a20eac358d0eac8e4c8170f2774e0e95
SunnyBarua <sunnybarua95@gmail.com>
2020-09-27 19:14:55 +0600

    brought changes

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 const { MONGOURI } = require("./keys");
 const User = require("./models/user");
 const Food = require("./models/food");
